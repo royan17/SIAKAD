73ffccf0e50e0b042acdec231d555ef8dcae1fdd
Your Name <royanhusen77@gmail.com>
2023-01-03 22:09:23 +0700

    first commit

---
+++ application/models/contoh_model.php
@@ -0,0 +1,11 @@
+<?php
+
+class Contoh_model extends CI_Model
+{
+    public function get_data()
+    {
+        
+    }
+}
+
+?>
---
+++ application/views/contoh_view.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contoh View</title>
+</head>
+<body>
+    <h1>Contoh view pada Codeigneter</h1>
+    <hr>
+    <p>Belajar Codeigneter di framework indonesia</p>
+</body>
+</html>
