97fcda5f43df8d5de0df34cebc48eadc798e6eb0
Your Name <royanhusen77@gmail.com>
2023-01-05 22:00:59 +0700

    add template

---
+++ application/controllers/mahasiswa.php
@@ -0,0 +1,104 @@
+<?php
+
+class Mahasiswa extends CI_Controller
+{
+    public function index()
+    {
+        $data['mahasiswa'] = $this->m_mahasiswa->tampil_data()->result();
+
+        $this->load->view('templates/header');
+		$this->load->view('templates/sidebar');
+		$this->load->view('mahasiswa', $data);
+		$this->load->view('templates/footer');
+    }
+
+    public function tambah_aksi(){
+        $nama      = $this->input->post('nama');
+        $nim       = $this->input->post('nim');
+        $tgl_lahir = $this->input->post('tgl_lahir');
+        $jurusan   = $this->input->post('jurusan');
+        $alamat = $this->input->post('alamat');
+        $email = $this->input->post('email');
+        $no_telp = $this->input->post('no_telp');
+
+        $data = array(
+            'nama' => $nama,
+            'nim' => $nim,
+            'tgl_lahir' => $tgl_lahir,
+            'jurusan' => $jurusan,
+            'alamat' => $alamat,
+            'email' => $email,
+            'no_telp' => $no_telp,
+        );
+
+        // var_dump($data); die();
+
+        $this->m_mahasiswa->input_data($data, 'tb_mahasiswa');
+        redirect('mahasiswa/index');
+    }
+
+    public function hapus($id)
+    {
+        $where = array('id' => $id);
+        $this->m_mahasiswa->hapus_data($where, 'tb_mahasiswa');
+        redirect('mahasiswa/index');
+    }
+
+    public function edit($id)
+    {
+        $where = array('id' => $id);
+        $data['mahasiswa'] = $this->m_mahasiswa->edit_data($where, 'tb_mahasiswa')->result();
+
+        $this->load->view('templates/header');
+		$this->load->view('templates/sidebar');
+		$this->load->view('edit', $data);
+		$this->load->view('templates/footer');
+    }
+
+    public function update() 
+    {
+        $id = $this->input->post('id');
+        $nama = $this->input->post('nama');
+        $nim = $this->input->post('nim');
+        $tgl_lahir = $this->input->post('tgl_lahir');
+        $jurusan = $this->input->post('jurusan');
+        $alamat = $this->input->post('alamat');
+        $email = $this->input->post('email');
+        $no_telp = $this->input->post('no_telp');
+
+        $data = array(
+            'nama' => $nama,
+            'nim' => $nim,
+            'tgl_lahir' => $tgl_lahir,
+            'jurusan' => $jurusan,
+            'alamat' => $alamat,
+            'email' => $email,
+            'no_telp' => $no_telp,
+        );
+
+        // var_dump($data); die();
+
+        $where = array(
+            'id' => $id,
+        );
+
+        $this->m_mahasiswa->update_data($where, $data, 'tb_mahasiswa');
+        redirect('mahasiswa/index');
+        
+    }
+
+    public function detail($id)
+    {
+        $this->load->model('m_mahasiswa');
+        $detail = $this->m_mahasiswa->detail_data($id);
+        $data['detail'] = $detail;
+
+        $this->load->view('templates/header');
+		$this->load->view('templates/sidebar');
+		$this->load->view('detail', $data);
+		$this->load->view('templates/footer');
+    }
+
+   
+
+}
---
+++ application/views/mahasiswa.php
@@ -0,0 +1,94 @@
+<div class="content-wrapper">
+    <section class="content-header">
+        <h1>
+            Data Mahasiswa
+            <small>Control panel</small>
+        </h1>
+        <ol class="breadcrumb">
+            <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+            <li class="active">Data Mahasiswa</li>
+        </ol>
+    </section>
+
+    <section class="content">
+        <button class="btn btn-primary" data-toggle="modal" data-target="#exampleModal"><i class="fa fa-plus"> </i>Tambah Data Mahasiswa</button>
+        <table class="table">
+            <tr>
+                <th>NO</th>
+                <th>NAMA MAHASISWA</th>
+                <th>NIM</th>
+                <th>TANGGAL LAHIR</th>
+                <th>JURUSAN</th>
+                <th colspan="2">AKSI</th>
+            </tr>
+            <?php
+            $no = 1;
+            foreach ($mahasiswa as $mhs): ?>
+                <tr>
+                    <td><?php echo $no++ ?> </td>
+                    <td><?php echo $mhs->nama ?></td>
+                    <td><?php echo $mhs->nim ?></td>
+                    <td><?php echo $mhs->tgl_lahir ?></td>
+                    <td><?php echo $mhs->jurusan ?></td>
+                    <td><?php echo anchor('mahasiswa/detail/' . $mhs->id, '<div class ="btn btn-success btn-sm"><i class="fa fa-search-plus"></i></div>') ?></td>
+                    <td onclick="javascript: return confirm('Anda Yakin Hapus!')"><?php echo anchor('mahasiswa/hapus/' . $mhs->id, '<div class="btn btn-danger btn-sm"><i class="fa fa-trash"></i></div>') ?></td>
+                    <td><?php echo anchor('mahasiswa/edit/' . $mhs->id, '<div class="btn btn-primary btn-sm"><i class="fa fa-edit"></i></div>') ?></td>
+                </tr>
+                <?php endforeach; ?>
+        </table>
+    </section>
+
+<!-- Modal -->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h4 class="modal-title" id="exampleModalLabel">FORM INPUT DATA MAHASISWA</h4>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+        <form method="post" action="<?php echo base_url() . 'mahasiswa/tambah_aksi'; ?>">
+                <div class="form-group">
+                    <label>Nama Mahasiswa</label>
+                    <input type="text" name="nama" class="form-control" >
+                </div>
+                <div class="form-group">
+                    <label>NIM</label>
+                    <input type="text" name="nim" class="form-control" >
+                </div>
+                <div class="form-group">
+                    <label>Tanggal Lahir</label>
+                    <input type="date" name="tgl_lahir" class="form-control" >
+                </div>
+                <div class="form-group">
+                    <label>Jurusan</label>
+                    <select class="form-control" name="jurusan">
+                        <option>Sistem Informasi</option>
+                        <option>Teknik Informatika</option>
+                        <option>Teknik Komputer</option>
+                    </select>
+                </div>
+                <div class="form-group">
+                    <label>Alamat</label>
+                    <input type="text" name="alamat" class="form-control" >
+                </div>
+                <div class="form-group">
+                    <label>Email</label>
+                    <input type="text" name="email" class="form-control" >
+                </div>
+                <div class="form-group">
+                    <label>No. Telp</label>
+                    <input type="number" name="no_telp" class="form-control" >
+                </div>
+
+        <button type="reset" class="btn btn-danger" data-dismiss="modal">Reset</button>
+        <button type="submit" class="btn btn-primary">Simpan</button>
+        </form>
+      </div>
+     
+    </div>
+  </div>
+</div>
+</div>
